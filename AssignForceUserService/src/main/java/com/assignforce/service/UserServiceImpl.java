54f948b4508b760d1edcea2620634faec8baea9c
Calvin Ly <clvnly@gmail.com>
2018-07-02 13:44:05 -0400

    initial push of userservice

---
+++ AssignForceUserService/src/main/java/com/assignforce/service/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.assignforce.service;
+
+import com.assignforce.models.User;
+import com.assignforce.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService{
+    @Autowired
+    UserRepository userRepo;
+
+    @Override
+    public List<User> getAll() {
+        return userRepo.findAll();
+    }
+
+    @Override
+    public User add(User user) {
+        return userRepo.save(user);
+    }
+
+    @Override
+    public User getById(int id) {
+        return userRepo.findOne(id);
+    }
+
+    @Override
+    public User getByUsername(String email) {
+        return userRepo.findUserByEmailLike(email);
+    }
+
+    @Override
+    public User update(User user) {
+        return userRepo.saveAndFlush(user);
+    }
+}
