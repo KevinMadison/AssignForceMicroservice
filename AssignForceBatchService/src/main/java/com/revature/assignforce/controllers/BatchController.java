b52d331a4325ee64139a278ab335fcb8fd408d06
NAshcroft42 <39060893+NAshcroft42@users.noreply.github.com>
2018-07-03 09:22:16 -0400

    trying to fix errors

--- AssignForceBatchService/src/main/java/com/revature/assignforce/controllers/BatchController.java
+++ AssignForceBatchService/src/main/java/com/revature/assignforce/controllers/BatchController.java
@@ -7,11 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.assignforce.beans.Batch;
